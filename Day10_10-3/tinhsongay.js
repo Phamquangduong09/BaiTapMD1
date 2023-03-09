eb67ac1c43040606243fe0b81caeb250a5667257
Phamquangduong09 <Phamquangduong009@gmail.com>
2023-03-09 11:22:30 +0700

    pushbaitap

---
+++ Day10_10-3/tinhsongay.js
@@ -0,0 +1,28 @@
+function tinhngay(){
+    let month = +document.getElementById("month").value;
+    console.log(month)
+    switch (month) {
+        case 1:
+            alert("tháng có 31 ngày");
+            break;
+        case 2:
+            alert("tháng có 28 hoặc 29 ngày");
+            break;
+        case 3:
+            alert("tháng có 31 ngày");;
+            break;
+        case 4:
+            alert("tháng có 30 ngày");;
+            break;
+        case 5:
+            alert("tháng có 31 ngày");;
+            break;
+        case 6:
+            alert("tháng có 30 ngày");;
+            break;
+        case 7:
+            alert("tháng có 31 ngày");;
+            break;
+    }
+
+}
