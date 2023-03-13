41eecceb0bd67a183e919cf274242cbfb40992bd
Phamquangduong09 <Phamquangduong009@gmail.com>
2023-03-13 15:21:30 +0700

    pushbaitap

--- Day8_8-3/Tinh_diem.js
+++
@@ -1,8 +0,0 @@
-let inputphysic = prompt("Enter the physic");
-let inputchemmitry = prompt("Enter the chemmitry");
-let inputbiology = prompt("Enter the biology");
-let physic = parseInt(inputphysic);
-let chemmitry = parseInt(inputchemmitry);
-let biology = parseInt(inputbiology);
-let area =( physic + chemmitry + biology) / 3;
-document.write("The area is: " + area);
--- Day8_8-3/tinh_doC.js
+++
@@ -1,3 +0,0 @@
- let inputC= prompt ("Enter the độ C");
-let f = 1.8* parseInt(inputC)+32;
-document.write("Độ F là: " +f);
